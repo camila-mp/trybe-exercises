a67ba1656686ad75e43cfbec2eb7ada6b10af234
camila-mp <ca.malvp@gmail.com>
2021-04-23 09:59:08 -0300

    corrige ex_4

---
+++ bloco_4/dia_1/ex_3.js
@@ -0,0 +1,13 @@
+let varA=17;
+
+let varB=10;
+
+let varC=5;
+
+if (varA > varB && varA > varC) {
+    console.log("varA");
+} else if (varB > varA && varB > varC){
+    console.log("varB");
+} else {
+    console.log("varC");
+};
