36cfc14f60e86829acd26f81df48622adaf91030
camila-mp <ca.malvp@gmail.com>
2021-04-23 11:14:07 -0300

    resolve ex_9

--- bloco_4/dia_1/ex_8.js
+++ bloco_4/dia_1/ex_8.js
@@ -1,12 +1,15 @@
-let a=20;
+let a=5;
 
 let b=13;
 
 let c=1;
 
+let isEven=false;
+
 if ((a%2===0) || (b%2===0) || (c%2===0)) {
-    console.log('true');
+    isEven=true
+    console.log(isEven);
 }
 else {
-    console.log ('false')
+    console.log (isEven)
 }
---
+++ bloco_4/dia_1/ex_9.js
@@ -0,0 +1,15 @@
+let a=50;
+
+let b=14;
+
+let c=8;
+
+let ehimpar=false;
+
+if ((a%2!==0) || (b%2!==0) || (c%2!==0)) {
+    ehimpar=true;
+    console.log(ehimpar);
+}
+else {
+    console.log (ehimpar);
+}
