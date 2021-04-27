1e072633cf6002c576ea05570095f4069c5bdcd8
camila-mp <ca.malvp@gmail.com>
2021-04-27 18:24:26 -0300

    resolve ex.4 4.4

--- bloco_4/dia_4/ex_4.js
+++ bloco_4/dia_4/ex_4.js
@@ -3,3 +3,9 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
+
+  info.recorrente = 'Sim';
+
+  for(key in info){
+      console.log(info[key])
+  }
