c1d5f39d36832bf5e12aa4da77cbda933cf5eaf0
camila-mp <ca.malvp@gmail.com>
2021-05-03 18:49:15 -0300

    resolve exercicio 1

--- bloco_5/dia_3/ex_praticar.js
+++ bloco_5/dia_3/ex_praticar.js
@@ -16,3 +16,23 @@ function createDaysOfTheWeek() {
   // Escreva seu código abaixo.
 
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function createDaysOfTheMonth () {
+      let ulDays = document.getElementById('days');
+      for (index = 0; index < dezDaysList.length; index += 1){
+        let day = document.createElement('li');
+        ulDays.appendChild(day);
+        day = dezDaysList[index];
+        day.className = 'day';
+
+        if (day === 24 || day === 25 || day === 31){
+            day.className = 'holiday';
+            if (day === 25){
+                day.className='friday';
+            }
+        }
+        if (day === 4 || day === 11 || day === 18){
+            day.className = 'friday'
+        } 
+      }
+  }
