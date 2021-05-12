b04e639b000e55220ae0deb5f646f2c090d166de
camila-mp <ca.malvp@gmail.com>
2021-05-11 21:14:08 -0300

    modifica html

--- bloco_6/dia_6.1/script.js
+++ bloco_6/dia_6.1/script.js
@@ -1,19 +1,17 @@
 //variaveis uteis//
-let dropdownEstado = document.getElementById('dropdown-estado');
+let dropdownEstado = document.getElementById('select-estado');
 
 let estadosBR = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
-//funcao que cria menu dropdown dos estados//
+//cria menu dropdown dos estados//
 
-function criaMenuEst() {
-  for (index = 0; index < estadosBR.length; index += 1){
-    let optionElement = document.createElement('option');
-    dropdownEstado.appendChild(optionElement);
-    optionElement.value = estadosBR[index];
-    optionElement.innerText = estadosBR[index];
-  };
+for (let index = 0; index < estadosBR.length; index += 1){
+  let optionElement = document.createElement('option');
+  dropdownEstado.appendChild(optionElement);
+  optionElement.value = estadosBR[index];
+  optionElement.innerText = estadosBR[index];
+  console.log('oi');
 };
 
-criaMenuEst();
 
 
