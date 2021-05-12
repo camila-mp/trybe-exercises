8f53ab422cd9c8c68fb7b02ec5672e72af17aa66
camila-mp <ca.malvp@gmail.com>
2021-05-12 18:28:09 -0300

    conserta o select dos estados

--- bloco_6/dia_6.1/script.js
+++ bloco_6/dia_6.1/script.js
@@ -3,14 +3,19 @@ let selectEstado = document.getElementById('select-estado');
 
 let estadosBR = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
-//cria menu dropdown dos estados//
+//cria tags option para selecionar estado//
 
-for (let index = 0; index < selectEstado.length; index += 1){
-  let opcaoSelect = document.createElement('option');
-  selectEstado.appendChild(opcaoSelect);
-  opcaoSelect.value = estadosBR[index];
-  opcaoSelect.innerText = estadosBR[index];
+function criaMenuEstados() {
+  for(let index = 0; index < estadosBR.length; index += 1){
+    let opcaoSelect = document.createElement('option');
+    selectEstado.appendChild(opcaoSelect);
+    opcaoSelect.value = estadosBR[index];
+    opcaoSelect.innerText = estadosBR[index];
+  };
 };
+
+criaMenuEstados();
+
 
 
 
