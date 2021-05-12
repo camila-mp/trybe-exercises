702972fb12af1acc2ac89c7912a695cd93560ff3
camila-mp <ca.malvp@gmail.com>
2021-05-12 09:56:36 -0300

    resolve ex. 1 e ex. 2 ate chegar na data de inicio

--- bloco_6/dia_6.1/script.js
+++ bloco_6/dia_6.1/script.js
@@ -1,16 +1,15 @@
 //variaveis uteis//
-let dropdownEstado = document.getElementById('select-estado');
+let selectEstado = document.getElementById('select-estado');
 
 let estadosBR = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
 //cria menu dropdown dos estados//
 
-for (let index = 0; index < estadosBR.length; index += 1){
-  let optionElement = document.createElement('option');
-  dropdownEstado.appendChild(optionElement);
-  optionElement.value = estadosBR[index];
-  optionElement.innerText = estadosBR[index];
-  console.log('oi');
+for (let index = 0; index < selectEstado.length; index += 1){
+  let opcaoSelect = document.createElement('option');
+  selectEstado.appendChild(opcaoSelect);
+  opcaoSelect.value = estadosBR[index];
+  opcaoSelect.innerText = estadosBR[index];
 };
 
 
