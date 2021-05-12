50e1b57bac020f00983866a0d5b821bf2931d1cf
camila-mp <ca.malvp@gmail.com>
2021-05-12 18:39:43 -0300

    adiciona arquivos

---
+++ bloco_6/dia_6.2/script.js
@@ -0,0 +1,21 @@
+//variaveis uteis//
+let selectEstado = document.getElementById('select-estado');
+
+let estadosBR = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+
+//cria tags option para selecionar estado//
+
+function criaMenuEstados() {
+  for(let index = 0; index < estadosBR.length; index += 1){
+    let opcaoSelect = document.createElement('option');
+    selectEstado.appendChild(opcaoSelect);
+    opcaoSelect.value = estadosBR[index];
+    opcaoSelect.innerText = estadosBR[index];
+  };
+};
+
+criaMenuEstados();
+
+
+
+
