103f23b867f514a70af2ab48f14fa89bd1c82599
camila-mp <ca.malvp@gmail.com>
2021-05-13 15:10:26 -0300

    remove bulma e adiciona bootstrap no html

--- bloco_6/dia_6.2/script.js
+++ bloco_6/dia_6.2/script.js
@@ -1,3 +1,4 @@
+
 //variaveis uteis//
 let selectEstado = document.getElementById('select-estado');
 
@@ -16,7 +17,26 @@ function criaMenuEstados() {
 
 criaMenuEstados();
 
+const options = {
+  dateFormat: 'DD/MM/YYYY',
+  color: 'link',
+};
 
+const calendars = bulmaCalendar.attach('#datepicker', options);
 
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to select event
+	calendar.on('select', date => {
+		console.log(date);
+	});
+});
 
-
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+};
