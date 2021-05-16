7d16f11a4f37d680d7aa29816798c573bfcb47a3
camila-mp <ca.malvp@gmail.com>
2021-05-16 20:59:48 -0300

    cria funçoes

--- bloco_6/dia_6.1/script.js
+++ bloco_6/dia_6.1/script.js
@@ -16,10 +16,91 @@ function criaMenuEstados() {
 
 criaMenuEstados();
 
+var isFormValid = false;
+
 const form = document.getElementById('form');
-const nameInput = document.getElementById('nome-compl');
+
+const nameInput= document.getElementById('nome-compl');
 const emailInput = document.getElementById('email');
-const cpfInput = document.querySelector('cpf');
+const cpfInput = document.getElementById('cpf');
+const userEmail = emailInput.value;
+
+const inputFields = [nameInput, emailInput, cpfInput];
+
+const descInputs = ["nome completo", "email", "CPF",
+];
+
+function inputExists() {
+  for (index = 0; index < inputFields.length; index +=1){
+    if (inputFields[index].value) {
+      inputFields[index].classList.add('valid');
+    }else{
+      inputFields[index].classList.add('invalid');
+    };
+  };
+};
+
+function checkCharacName() {
+  nameInput.className = '';
+  if (nameInput.value.length > 40){
+    nameInput.className = 'invalid';
+  }else{
+    nameInput.className = 'valid';
+  };
+};
+
+function checkEmail() {
+  emailInput.className = '';
+  var validateEmail = /\S+@\S+\.\S+/.test(userEmail);
+  if (emailInput.value.length > 50 || validateEmail === false){
+    emailInput.className = 'invalid';
+  }else{
+    emailInput.className = 'valid';
+    console.log('oi')
+  };
+};
+
+function checkCharacCpf(){
+  cpfInput.className = '';
+  if (cpfInput.value === 11){
+    cpfInput.className = 'valid';
+  }else{
+    cpfInput.className = 'invalid';
+  };
+};
+
+function createInvalidList() {
+  var newArray = [];
+  for (index2= 0; index2 < descInputs.length; index2 += 1){
+    if (inputFields[index2].className === 'invalid'){
+      newArray.push(descInputs[index2]);
+    };
+  };
+  if (newArray !== []) {
+    var errorDiv = document.createElement('div');
+    body.appendChild(errorDiv);
+    errorDiv.className = 'errorDiv';
+    for (index of newArray){
+      errorDiv.innerText = 'O ' + newArray[index] + ' inserido é inválido.\n'
+    }; 
+  };
+};
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  inputExists();
+  for (index1= 0; index1 < inputFields.length; index1 += 1){
+    if (inputFields[index1].className === 'invalid'){
+      alert("Por favor preencha todos os campos obrigatórios!")
+    };
+  };
+  checkCharacName();
+  checkEmail();
+  checkCharacCpf();
+  createInvalidList();
+  console.log('hello')
+});
+
 
 
 
