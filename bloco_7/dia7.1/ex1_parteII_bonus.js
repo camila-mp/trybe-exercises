c01f55b67f1ea590e50ccf1d70a6ebffbd43865d
camila-mp <ca.malvp@gmail.com>
2021-05-19 19:40:09 -0300

    tentando resolver o bonus ex. 1

--- bloco_7/dia7.1/ex1_parteII_bonus.js
+++ bloco_7/dia7.1/ex1_parteII_bonus.js
@@ -1,9 +1,6 @@
 const retornaFatorial = (num) => {
-    let guardaNum = 1;
-    while (num >= 1){
-      guardaNum = guardaNum * num;
-      num -= 1; }
-    return console.log(guardaNum);
-  }
+  let fatorial = 1 * (num);
+  
+}
   
   retornaFatorial(4);
