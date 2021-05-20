13b32c533ebbe7f1d5d720e11786ebdbe13d5b0a
camila-mp <ca.malvp@gmail.com>
2021-05-20 18:26:59 -0300

    resolve ex. 1, provavelmente usando o metodo mais complicado e deselegante que existe kkk

---
+++ bloco_7/dia7.2/ex1_parteI.js
@@ -0,0 +1,53 @@
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        margherita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+ console.log(order.order.delivery.deliveryPerson)
+
+//  "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701"
+
+  const customerInfo = (order) => {
+    
+  
+  }
+  
+  customerInfo(order);
+  
+  const orderModifier = (order) => {
+    // Adicione abaixo as informações necessárias.
+  
+  }
+  
+  orderModifier(order);
