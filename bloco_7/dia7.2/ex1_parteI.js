1d6b3642c76a2b2e304e51c4ae9c662259668254
camila-mp <ca.malvp@gmail.com>
2021-05-20 18:30:43 -0300

    exercicio 1 parte I resolvido

--- bloco_7/dia7.2/ex1_parteI.js
+++ bloco_7/dia7.2/ex1_parteI.js
@@ -34,13 +34,15 @@ const order = {
     },
   };
   
- console.log(order.order.delivery.deliveryPerson)
-
 //  "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701"
 
   const customerInfo = (order) => {
-    
-  
+    let deliveryPerson = order.order.delivery.deliveryPerson;
+    let customer = order.name;
+    let telefone = order.phoneNumber;
+    let adress = Object.values(order.address)
+    let fraseFinal = `Olá ${deliveryPerson}, entrega para: ${customer}, Telefone: ${telefone}, R. ${adress[0]}, Num: ${adress[1]}, AP: ${adress[2]}`
+    console.log(fraseFinal);
   }
   
   customerInfo(order);
