48ff6982156491431c12a79552dacc49cada6fd8
camila-mp <ca.malvp@gmail.com>
2021-05-23 18:22:25 -0300

    adiciona ex2 parte II

--- bloco_7/dia7.2/ex2_parteII.js
+++ bloco_7/dia7.2/ex2_parteII.js
@@ -1,1 +1,16 @@
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listaKeys = (objeto) => {
+    let keys = Object.keys(objeto)
+    return keys;
+};
+
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+console.log(listaKeys(lesson3));
+
