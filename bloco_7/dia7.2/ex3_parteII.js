3467422c95f82789456bae75db89b3b4c74cee3a
camila-mp <ca.malvp@gmail.com>
2021-05-23 19:22:04 -0300

    resolve ex3 parte II

--- bloco_7/dia7.2/ex3_parteII.js
+++ bloco_7/dia7.2/ex3_parteII.js
@@ -1,1 +1,18 @@
 // Crie uma função para mostrar o tamanho de um objeto.
+
+const objectSize = (object) => {
+    let arrayEntries = Object.entries(object);
+    let objSize = arrayEntries.length;
+    return objSize;
+}
+
+// teste
+
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+console.log(objectSize(lesson3));
