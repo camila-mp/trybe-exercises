3d4f1cf8809bfc0914ca9a15f6ddcacdc69fcadf
camila-mp <ca.malvp@gmail.com>
2021-05-23 16:46:08 -0300

    refatorando ex.2 parte II

--- bloco_7/dia7.1/ex2_parteII.js
+++ bloco_7/dia7.1/ex2_parteII.js
@@ -2,22 +2,14 @@
 
 const achaMaiorPalav = (str) => {
     let arrayPalavras = str.split(' ');
-    let novoArrayN = [];
-    let novoArrayP = [];
-    for (index = 0; index < arrayPalavras.length; index += 1){
-        let palavra = arrayPalavras[index];
-        novoArrayP.push(palavra);
-        let numLetras = palavra.split('').length;
-        novoArrayN.push(numLetras);
-    }
-    let maiorNum = novoArrayN[0];
-    for (index2 = 1; index2 < novoArrayN.length; index2 += 1){
-        if(maiorNum < novoArrayN[index2]){
-            maiorNum = novoArrayN[index2];
+    let maiorComp = 0;
+    let maiorPalavra = '';
+    for (const word of arrayPalavras){
+        if(word.length > maiorComp){
+            maiorComp = word.length;
+            maiorPalavra = word;
         }
-    }
-    let indexMaiorP = novoArrayN.indexOf(maiorNum);
-    let maiorPalavra = novoArrayP[indexMaiorP];
+    };
     return maiorPalavra;
 };   
 
