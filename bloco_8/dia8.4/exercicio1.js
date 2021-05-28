9978b8072db42635cfdfdab3910f18998b962a47
camila-mp <ca.malvp@gmail.com>
2021-05-28 18:11:03 -0300

    adiciona exercicios 1, 2 e 3

---
+++ bloco_8/dia8.4/exercicio1.js
@@ -0,0 +1,15 @@
+// Dada uma matriz de matrizes, transforme em uma única matriz.
+
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
