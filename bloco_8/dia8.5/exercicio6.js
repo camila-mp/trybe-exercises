7a5626361d59efdb98048d7b855ea37dcb1049d6
camila-mp <ca.malvp@gmail.com>
2021-06-01 14:31:49 -0300

    resolve ex 5 e 6

--- bloco_8/dia8.5/exercicio6.js
+++ bloco_8/dia8.5/exercicio6.js
@@ -8,6 +8,8 @@ const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
 
+const toObject = ([modelo, marca, ano]) => ({ name: `${modelo}`, brand: `${marca}`, year: ano });
+
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
 assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
