0539684462ff5e7d42fc31d46e9a4f74c341881e
camila-mp <ca.malvp@gmail.com>
2021-06-16 18:10:58 -0300

    termina ex 4

--- bloco_10/10.2/exercicio4.test.js
+++ bloco_10/10.2/exercicio4.test.js
@@ -4,8 +4,8 @@ const getRepos = require('./exercicio4');
 
 test('teste que verifica se 2 respositorios estão contidos na resposta recebida', () => {
     expect.assertions(2);
-    return getRepos('https://api.github.com/orgs/tryber/repos')
-    .then((data) => {
+    const url = 'https://api.github.com/orgs/tryber/repos';
+    return getRepos(url).then((data) => {
         expect(data).toContain('sd-01-week4-5-project-todo-list');
         expect(data).toContain('sd-01-week4-5-project-meme-generator');
     });
