e2d57821aa63febe92765fbbb91b30ac118b95a3
camila-mp <ca.malvp@gmail.com>
2021-06-22 21:24:02 -0300

    adiciona primeiro componente (pokemon)

---
+++ bloco_11/dia11.2/my-pokedex/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Pokemon from './pokemon';
+
+class App extends React.Component {
+  render() {
+    return <Pokemon />;
+  }
+}
+
+export default App;
