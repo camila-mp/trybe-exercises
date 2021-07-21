3b1baf170b118461f512ecd9dc07fa841f6636e5
camila-mp <ca.malvp@gmail.com>
2021-07-20 22:42:02 -0300

    adiciona mais testes em App

---
+++ exercise15.2-digimon-finders/src/App.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+import Digimon from './Digimon';
+
+describe('Teste da aplicação toda', () => {
+  it('Verificando se botão com texto search digimon existe', async () => {
+    render(<App />);
+    const searchBtn = screen.getByText(/search digimon/i);
+    expect(searchBtn).toBeInTheDocument();
+    expect(searchBtn.type).toBe('button');
+  });
+  it('Verificando se input de digimon existe', () => {
+    render(<App />);
+    const digimonInput = screen.getByTestId('input');
+    expect(digimonInput.type).toBe('text');
+  })
+  it('verifica comportamento da aplicação quando é feita busca por digimon', async () => {
+    const fetchResponse = [{
+      name: 'Agumon',
+      img: 'https://digimon.shadowsmith.com/img/agumon.jpg',
+      level: 'Rookie',
+    }]
+
+    global.fetch = jest.fn( async () => 
+      ({
+        json: async () => fetchResponse,
+      }))
+    render(<App />, <Digimon />);
+    const searchBtn = screen.getByTestId('buttonSearch');
+    const digimonInput = screen.getByTestId('input');
+    // const digimonImg = screen.findByAltText('Agumon')
+    fireEvent.change(digimonInput, { target: { value: 'agumon' } });
+    fireEvent.click(searchBtn);
+    await screen.findByText('Agumon');
+    expect(screen.getByTestId('digimonName')).toBeInTheDocument();
+    expect(screen.getByAltText('Agumon')).toBeInTheDocument();
+    expect(global.fetch).toBeCalledWith('https://digimon-api.vercel.app/api/digimon/name/agumon');
+    expect(global.fetch).toBeCalledTimes(1);
+  })
+});
